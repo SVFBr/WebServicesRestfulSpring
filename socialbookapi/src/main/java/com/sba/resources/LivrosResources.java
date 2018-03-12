7483c283c78595fefbccbc69e4bfdd9fecab76bb
SVFBr <sheilafreire.br@gmail.com>
2018-03-12 20:24:11 -0300

    V 0.2 -> ResponseEntity

--- socialbookapi/src/main/java/com/sba/resources/LivrosResources.java
+++ socialbookapi/src/main/java/com/sba/resources/LivrosResources.java
@@ -1,14 +1,18 @@
 package com.sba.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sba.domain.Livro;
 import com.sba.repository.LivrosRepository;
@@ -26,8 +30,10 @@ public class LivrosResources {
 	 * 
 	 */
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		livrosRepository.save(livro);
+	public ResponseEntity<?> salvar(@RequestBody Livro livro) {
+		livro = livrosRepository.save(livro);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livro.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	/**
@@ -36,8 +42,8 @@ public class LivrosResources {
 	 * 
 	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Livro> listar() {
-		return livrosRepository.findAll();
+	public ResponseEntity<List<Livro>> listar() {
+		return ResponseEntity.status(HttpStatus.OK).body(livrosRepository.findAll());
 	}
 
 	/**
@@ -45,10 +51,13 @@ public class LivrosResources {
 	 * BUSCAR
 	 * 
 	 */
-
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Optional<Livro> listar(@PathVariable("id") Long id) {
-		return livrosRepository.findById(id);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Livro livro = livrosRepository.findOne(id);
+		if (livro == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(livro);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 
 	/**
@@ -57,9 +66,10 @@ public class LivrosResources {
 	 * 
 	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public void atualizar(@PathVariable("id") Long id, @RequestBody Livro livro) {
+	public ResponseEntity<Void> atualizar(@PathVariable("id") Long id, @RequestBody Livro livro) {
 		livro.setId(id);
 		livrosRepository.save(livro);
+		return ResponseEntity.noContent().build();
 	}
 
 	/**
@@ -67,9 +77,14 @@ public class LivrosResources {
 	 * DELETAR
 	 * 
 	 */
- 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void deletar(@PathVariable("id") Long id) {
-		livrosRepository.deleteById(id);
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+		try {
+			livrosRepository.delete(id);
+		} catch (EmptyResultDataAccessException e) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- socialbookapi/src/test/java/com/sba/SocialbookapiApplicationTests.java
+++ socialbookapi/src/test/java/com/sba/SocialbookapiApplicationTests.java
@@ -2,11 +2,13 @@ package com.sba;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SocialbookapiApplicationTests.class)
+@WebAppConfiguration
 public class SocialbookapiApplicationTests {
 
 	@Test
